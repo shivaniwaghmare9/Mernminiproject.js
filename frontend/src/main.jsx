79a8cfb02edcbf5ae71d84b717218920f804dd6e
shivani <shivaniw69@gmail.com>
2025-08-04 01:20:42 +0530

    css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import "./CSS/style.css"
 createRoot(document.getElementById('root')).render(
  
     <App />
