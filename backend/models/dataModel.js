341d54fb79bf514a1f5f4df230c50baf494ca44e
shivani <shivaniw69@gmail.com>
2025-08-04 02:13:59 +0530

    model

---
+++ backend/models/dataModel.js
@@ -0,0 +1,9 @@
+
+const mongoose=require("mongoose");
+const dataSchema=new mongoose.Schema({
+    name:String,
+    rollno:Number,
+    subject:String,
+    fees:Number
+})
+module.exports=mongoose.model("student",dataSchema);
