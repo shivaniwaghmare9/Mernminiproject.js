58120786c34b0f4ee3b3c03c07e3ae0be954c8f2
shivani <shivaniw69@gmail.com>
2025-08-04 02:11:12 +0530

    inddex

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 
 const express=require("exprss");
 const app=express();
+const dataRoute=require("./routes/dataRoute")
 
+
+app.use("students",dataRoute)
 app.listen(3000,()=>{
     console.log("Server is running on port 3000")
 })
