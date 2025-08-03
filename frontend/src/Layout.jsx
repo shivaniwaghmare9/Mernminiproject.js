de2a123704e62f691c3b7efba2934ad99486bb65
shivani <shivaniw69@gmail.com>
2025-08-04 01:17:13 +0530

    layout

--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -1,9 +1,15 @@
 
 import Outlet from "react-router-dom" 
+import Header from "./Components/Header";
+import Topnav from "./Components/Topnav";
+import Footer from "./Components/Footer";
 const Layout=()=>{
     return(
         <>
+        <Header/>
+        <Topnav/>
          <Outlet/>
+         <Footer/>
         </>
     )
 }
