25e73e95fe60121be03d0a33c36edfdea27d8fe1
shivani <shivaniw69@gmail.com>
2025-08-04 01:43:27 +0530

    home

--- frontend/src/Components/Topnav.jsx
+++ frontend/src/Components/Topnav.jsx
@@ -2,17 +2,24 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 const Topnav=()=>{
     return(
         <>
         <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            
+          <Navbar.Brand href="#home">My site</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link as={Link} to="home">Home</Nav.Link>
+            <Nav.Link as={Link} to="insert">Insert</Nav.Link>
+            <Nav.Link as={Link} to="display">Display</Nav.Link>
+            <Nav.Link as={Link} to="update">Update</Nav.Link>
+            <Nav.Link as={Link} to="search">Search</Nav.Link>
+            <Nav.Link as={Link} to="contact">Contact</Nav.Link>
+           
           </Nav>
+         
         </Container>
       </Navbar>
         </>
--- frontend/src/Layout.jsx
+++ frontend/src/Layout.jsx
@@ -1,5 +1,5 @@
 
-import Outlet from "react-router-dom" 
+import {Outlet} from "react-router-dom" 
 import Header from "./Components/Header";
 import Topnav from "./Components/Topnav";
 import Footer from "./Components/Footer";
