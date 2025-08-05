9feabc80a27b1e2d1c6114bc1b514803ed8e416e
shivani <shivaniw69@gmail.com>
2025-08-06 02:15:39 +0530

    search

---
+++ frontend/src/Pages/Search.jsx
@@ -0,0 +1,11 @@
+
+const Search=()=>{
+    return(
+        <>
+        <h3 className="h33">Search Data!!!</h3>
+        Enter Rollno: <input type="text" /><br/><br/>
+        <button className="button">Search</button>
+        </>
+    )
+}
+export default Search;
