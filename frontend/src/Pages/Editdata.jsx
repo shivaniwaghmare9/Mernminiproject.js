5cf10a00726849925458f292097244d7a916997a
shivani <shivaniw69@gmail.com>
2025-08-05 23:46:37 +0530

    edit

--- frontend/src/Pages/Editdata.jsx
+++ frontend/src/Pages/Editdata.jsx
@@ -3,30 +3,43 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import {ToastContainer,toast} from "react-toastify"
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import BackendUrl from '../Utils/BackendURL';
+import axios from 'axios';
 const Editdata=()=>{
     const {id}=useParams();
+    const [mydata,setMydata]=useState({});
+    const loadData=async()=>{
+        let api=`${BackendUrl}/students/?id=${id}`;
+        const response=await axios.get(api,mydata);
+        console.log(response.data);
+        setMydata(response.data);
+    }
+    useEffect(()=>{
+        loadData();
+    },[])
     return(
         <>
         <h3>Edit data {id}</h3>
         <Form id="form">
       <Form.Group className="mb-3" >
         <Form.Label>Name</Form.Label>
-        <Form.Control type="text"  name="name" onChange={handleInput}/>
+        <Form.Control type="text"  name="name" value={mydata.name} onChange={handleInput}/>
      </Form.Group>
 
       <Form.Group className="mb-3" >
         <Form.Label>Rollno</Form.Label>
-        <Form.Control type="text"  name="rollno" onChange={handleInput}/>
+        <Form.Control type="text"  name="rollno" value={mydata.rollno} onChange={handleInput}/>
      </Form.Group>
 
       <Form.Group className="mb-3" >
         <Form.Label>Subject</Form.Label>
-        <Form.Control type="text"  name="subject" onChange={handleInput}/>
+        <Form.Control type="text"  name="subject" value={mydata.subject} onChange={handleInput}/>
      </Form.Group>
 
      <Form.Group className="mb-3" >
         <Form.Label>Fees</Form.Label>
-        <Form.Control type="text"  name="fees" onChange={handleInput}/>
+        <Form.Control type="text"  name="fees" value={mydata.fees} onChange={handleInput}/>
      </Form.Group>
       <Button variant="primary" type="submit" onClick={handleSubmit}>
         Submit
