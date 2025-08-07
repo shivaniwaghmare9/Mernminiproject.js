a4f38f88612b3384809cfdb261219f59ee152d76
shivani <shivaniw69@gmail.com>
2025-08-07 15:06:35 +0530

    search

--- frontend/src/Pages/Search.jsx
+++ frontend/src/Pages/Search.jsx
@@ -1,16 +1,22 @@
 
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { useState } from 'react';
 const Search=()=>{
+  const[rno,setRno]=useState("");
+
+  const handleSubmit=()=>{
+    
+  }
     return(
         <>
         <h3 className="h33">Search Data!!!</h3>
     <Form id="form">
       <Form.Group className="mb-3" >
         <Form.Label>Enter Name</Form.Label>
-        <Form.Control type="text"  name="name"/>
+        <Form.Control type="text"  value={rno} onChange={(e)=>{setRno(e.target.value)}}/>
      </Form.Group>
-     <Button variant="primary" type="button">
+     <Button variant="primary" type="button" onClick={handleSubmit}>
         Search
       </Button>
     </Form>
