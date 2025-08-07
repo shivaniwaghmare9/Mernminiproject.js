390fda4d1bb3931f59ed28273f2ab15035ca41c3
shivani <shivaniw69@gmail.com>
2025-08-07 16:05:25 +0530

    home

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,4 +1,31 @@
 
+// import img1 from "../images/img1.jpeg"
+// const Home=()=>{
+//     return(
+//         <>
+//     <h3 className='h33'>Our page!!</h3>
+//           <div class="container">
+    
+//     <div class="text">
+//       <h1>Welcome to Our Website</h1>
+//       <p>
+//         We provide creative digital solutions to grow your business online.
+//         Let’s build something amazing together. Check out our services and portfolio to learn more.
+//       </p>
+//       <button>Get Started</button>
+//     </div>
+
+    
+//     <div class="image">
+//       <img src={img1} alt="Home page illustration"/>
+//     </div>
+//   </div>
+//         </>
+//     )
+// }
+// export default Home
+
+
 import img1 from "../images/img1.jpeg"
 const Home=()=>{
     return(
